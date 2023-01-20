81de02117cffa8094c3fff95f3f4d074d77dbe60
jmlw8023 <jmlw8023@163.com>
2023-01-20 12:30:31 +0800

    opencv start

---
+++ opencv/img_base/open_show_img.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   open_show_img.py
+@Time    :   2023/01/20 12:29:55
+@Author  :   jolly 
+@Version :   python3.8
+@Contact :   jmlw8023@163.com
+@Reference_Linking  :  https://github.com/jmlw8023/coding
+'''
+
+# import packets
+# import os
+import cv2 as cv
+
+
+
+
+
+
+
