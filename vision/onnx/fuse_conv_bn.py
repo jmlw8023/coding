3dcb60dce1832901f78234b42cc07b6c9592022e
jmlw8023 <jmlw8023@163.com>
2023-02-09 22:26:19 +0800

    onnx

---
+++ vision/onnx/fuse_conv_bn.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   fuse_conv_bn.py
+@Time    :   2023/02/08 16:56:12
+@Author  :   jolly 
+@Version :   python3.8
+@Contact :   jmlw8023@163.com
+@Reference_Linking  :  https://github.com/jmlw8023/coding
+'''
+
+# import packets
+import os
+import onnx
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
