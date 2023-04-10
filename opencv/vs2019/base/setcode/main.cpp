cece4386a8094ccdf53ec7f699e0d7f235e6ea7c
jmlw8023 <jmlw8023@163.com>
2023-04-10 17:28:24 +0800

    opencv use c++

--- opencv/vs2019/base/setcode/main.cpp
+++ opencv/vs2019/base/setcode/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-
+#include <opencv2/dnn.hpp>
 
 using namespace std;
 using namespace cv;
@@ -12,8 +12,10 @@ int main(int argc, char** argv)
 	string imgPath = "E:/source/code/coding/opencv/data/images/milk1.jpg";
 	Mat img = imread(imgPath);
 
-
+	namedWindow("milk", WINDOW_AUTOSIZE);
 	imshow("milk", img);
+
+
 
 	waitKey(0);
 	destroyAllWindows();
