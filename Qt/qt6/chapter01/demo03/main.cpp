dcf0aa0101817dfd93486de65cd489152da6fe82
jmlw8023 <jmlw8023@163.com>
2023-04-10 10:14:30 +0800

    add

--- Qt/qt6/chapter01/demo03/main.cpp
+++ Qt/qt6/chapter01/demo03/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <vector>
+#include <list>
 
 using namespace std;
 
@@ -21,10 +22,22 @@ int main(int argc, char const *argv[])
     PrintVector(v1);
 
     vector<int> v2(5, 10);
+    PrintVector(v2);
 
+    cout <<  "v2.size() " << v2.size() << endl;
+    cout << "v2.capacity() " << v2.capacity()  << endl;
 
-      PrintVector(v2);
-
+    vector<int> v3(v1.begin(), v1.end());
+    PrintVector(v3);
+    v3.insert(v3.begin(), 22);
+    PrintVector(v3);
+    v3.insert(v3.begin(), 3, 100);
+    // 尾插
+    v3.push_back(88);
+//    PrintVector(v3);
+//    PrintVector(v1);
+    // 交换两个vector 元素
+    v3.swap(v1);
 
     return 0;
 }
---
+++ opencv/vs2019/base/setcode/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char** argv)
+{
+
+	string imgPath = "E:/source/code/coding/opencv/data/images/milk1.jpg";
+	Mat img = imread(imgPath);
+
+
+	imshow("milk", img);
+
+	waitKey(0);
+	destroyAllWindows();
+
+	//cout << "cpp and opencv using in vs2019!!" << endl;
+
+
+
+	return 0;
+}
+
