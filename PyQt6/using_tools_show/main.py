1374e87970130aec05c5847b3081c82264a78d31
jmlw8023 <jmlw8023@163.com>
2024-05-28 17:30:30 +0800

    updata

--- PyQt6/using_tools_show/main.py
+++ PyQt6/using_tools_show/main.py
@@ -16,7 +16,7 @@ import sys
 import cv2 as cv
 import numpy as np
 from PyQt6.QtCore import Qt, pyqtSignal, QUrl
-from PyQt6.QtWidgets import (QApplication, QMainWindow, QFileDialog, QMessageBox)
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QFileDialog, QMessageBox, QLabel)
 from PyQt6.QtGui import QDesktopServices, QImage, QPixmap, QFont, QResizeEvent
 
 from ui.ui_home import Ui_MainWindow
@@ -59,10 +59,12 @@ class Home_win(QMainWindow):
         self.im = None
         self.img_show = False
         self.img_path = None
+        
         self.yolo_img_path = None
-        self.yolo_weight_path = None
         self.model = None
         self.yolo_image = None
+        self.yolo_im = None
+        self.is_img_yolo_detecting = False
         
         self.CLASSES = ['addB', 'addS', 'pitN', 'pitF']
         self.random_colors = np.random.uniform(0, 255, size=(len(self.CLASSES), 3))
@@ -84,10 +86,16 @@ class Home_win(QMainWindow):
         self.ui.btn_home_count.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(1))
         self.ui.btn_home_yolo.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(2))
         self.ui.btn_home_other.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(3))
+        
         ##################################################################################################
         ##################################################################################################
         
         # self.ui.btn_side.setText('<<')
+        
+        self.ui.spinBox_thresh.setFixedWidth(80)
+        
+        self.yolo_weight_path = r'./data/weights/b113_yolov8n.onnx'
+        self.ui.lineEdit_yolo_weight_path.setText(self.yolo_weight_path)
 
 
     # 初始化 信号与槽
@@ -96,19 +104,27 @@ class Home_win(QMainWindow):
         ##################################################################################################
         self.ui.btn_yolo_open_file.clicked.connect(self.open_yolo_file)
         self.ui.btn_yolo_open_weight.clicked.connect(self.open_yolo_weight)
-        self.ui.btn_yolo_detect.clicked.connect(self.yolo_detect)
+        self.ui.btn_yolo_detect.clicked.connect(self.click_yolo_detect)
         self.ui.btn_yolo_save.clicked.connect(self.save_yolo_detect)
         self.ui.btn_yolo_save_file.clicked.connect(self.open_save_yolo_folder)
+        
+        self.ui.btn_yolo_big_show.setCheckable(True)
+        self.ui.btn_yolo_big_show.clicked.connect(self.yolo_show_big_img)
+        
+        
+        
+        
+        self.ui.btn_yolo_test.clicked.connect(self.open_yolo_test)
         
         ##################################################################################################
         self.ui.btn_open_img.clicked.connect(self.open_img_path)       
         # self.ui.btn_v_play.clicked.connect(self.detect_img)       
         self.ui.btn_v_clear.clicked.connect(self.clear_page)       
         
-        self.ui.spinBox.valueChanged.connect(self.ui.horizontalSlider.setValue)     # spinbox信号发送给滑竿
+        self.ui.spinBox_thresh.valueChanged.connect(self.ui.horizontalSlider.setValue)     # spinbox信号发送给滑竿
 
         self.ui.horizontalSlider.valueChanged.connect(self.threshold_set_img)
-        self.ui.horizontalSlider.valueChanged.connect(self.ui.spinBox.setValue)
+        self.ui.horizontalSlider.valueChanged.connect(self.ui.spinBox_thresh.setValue)
         
     # 选择 权重文件
     def open_yolo_weight(self):
@@ -132,7 +148,7 @@ class Home_win(QMainWindow):
                 QMessageBox.information(self, '信息', '请先载入权重文件!')
                 
         else:
-            self.ui.lineEdit_video_path.clear()
+            self.ui.lineEdit_yolo_weight_path.clear()
             QMessageBox.information(self, '错误', '请先载入正确路径!')
             
             
@@ -158,20 +174,105 @@ class Home_win(QMainWindow):
                 QMessageBox.information(self, '信息', '请先载入图像文件!')
                 
         else:
-            self.ui.lineEdit_video_path.clear()
+            self.ui.lineEdit_yolo_file_path.clear()
             QMessageBox.information(self, '错误', '请先载入正确路径!')
             
             
-    def yolo_detect(self):
+            
+    # 选择图像文件
+    def open_yolo_test(self):
         
-        if self.model is not None and self.yolo_image is not None:
-            self.yolo_im = self.yolo_image.copy()
-            h, w = self.yolo_image.shape[:2]
+        # file_path = QFileDialog.getOpenFileName(self, '选择图片', './', 'All Files(*);;*.jpg;;*.png;;')
+        # yolo_img_path = file_path[0]
+        # if yolo_img_path is not None and len(yolo_img_path) > 0:
+
+        #     if yolo_img_path.lower().endswith(tuple(self.img_format_lists)):
+                
+        #         print(yolo_img_path)
+        #         # self.ui.label_yolo_test.setPixmap(QPixmap(yolo_img_path).scaled(self.width()//2, int(self.height()/1.3)))
+    
+        #         image: np.ndarray = cv.imread(yolo_img_path)
+                
+        #         pixmap = QPixmap.fromImage(cv_mat_to_qimage(image))
+        #         self.ui.label_yolo_test.setPixmap(pixmap.scaled(self.width()//2, int(self.height()/1.3)))
+            
+        #     else:
+        #         QMessageBox.information(self, '信息', '请先载入图像文件!') 
+        # else:
+        #     QMessageBox.information(self, '错误', '请先载入正确路径!')
+            
+        
+        img_path = r'D:/source/code/datasets/data_b113/train/images/100081_555.png'
+        image: np.ndarray = cv.imread(img_path)
+        
+        h, w = image.shape[:2]
+        print(w, h)
+        cv.rectangle(image, (300, 100), (w-300, h-100), (0, 0, 230), 3)
+        
+        
+        
+        pixmap = QPixmap.fromImage(cv_mat_to_qimage(image))
+        self.ui.label_yolo_test.setPixmap(pixmap.scaled(self.width()//2, int(self.height()/1.3)))
+        
+        
+    def yolo_show_big_img(self, btn_flag):
+        
+        if btn_flag:
+            
+            self.label_img_big = QLabel()
+            self.label_img_big.setWindowTitle('结果放大')
+            self.label_img_big.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            # if self.is_img_yolo_detecting:
+            if self.yolo_im is not None:
+                pixmap = QPixmap.fromImage(cv_mat_to_qimage(self.yolo_im))
+                # self.label_img_big.setPixmap(pixmap.scaled(self.ui.label_v_src.size()))
+                self.label_img_big.setPixmap(pixmap.scaled(self.width(), int(self.height())))
+                self.label_img_big.show()
+                self.is_img_yolo_detecting = True
+                
+                self.label_img_big.destroyed.connect(self.destroy_big_label)
+                    
+            else:
+                self.ui.btn_yolo_big_show.setChecked(False)
+                QMessageBox.critical(self, '提示', '未有已检测图像!')
+            
+        # else:
+            
+            
+        
+    def destroy_big_label(self):
+        print('---------destroy_big_label------------')
+        self.ui.btn_yolo_big_show.setChecked(False)
+        self.is_img_yolo_detecting = False
+            
+    def click_yolo_detect(self):
+         
+        if self.yolo_image is not None:
+            yolo_im = self.yolo_detect(self.yolo_image.copy())
+          
+            pixmap = QPixmap.fromImage(cv_mat_to_qimage(yolo_im))
+            # self.ui.label_v_src.setPixmap(pixmap.scaled(self.ui.label_v_src.size()))
+            self.ui.label_yolo_dst.setPixmap(pixmap.scaled(self.width()//2, int(self.height()/1.3)))
+            
+            if self.is_img_yolo_detecting:
+                self.label_img_big.setPixmap(pixmap.scaled(self.width(), int(self.height())))
+        else:
+            QMessageBox.information(self, '信息', '请先载入图像文件!')
+    def yolo_detect(self, yolo_image):
+        
+        if not self.model: 
+            print(self.yolo_weight_path)
+            if self.yolo_weight_path.lower().endswith('.onnx'):
+                self.model: cv.dnn.Net = cv.dnn.readNetFromONNX(self.yolo_weight_path)
+                  
+        if yolo_image is not None:
+            self.yolo_im = yolo_image
+            h, w = self.yolo_im.shape[:2]
 
             length = max((h, w))
 
             image = np.zeros((length, length, 3), np.uint8)
-            image[0:h, 0:w] = self.yolo_image
+            image[0:h, 0:w] = self.yolo_im
 
             scale = length / 640
 
@@ -221,20 +322,27 @@ class Home_win(QMainWindow):
                     "scale": scale,
                 }
                 detections.append(detection)
-                self.draw_bounding_box(
-                    self.yolo_im,
-                    class_ids[index],
-                    scores[index],
-                    round(box[0] * scale),
-                    round(box[1] * scale),
-                    round((box[0] + box[2]) * scale),
-                    round((box[1] + box[3]) * scale),
-                )
-
+                # self.draw_bounding_box(
+                #     self.yolo_im,
+                #     class_ids[index],
+                #     scores[index],
+                #     round(box[0] * scale),
+                #     round(box[1] * scale),
+                #     round((box[0] + box[2]) * scale),
+                #     round((box[1] + box[3]) * scale),
+                # )
+                x = round(box[0] * scale)
+                y = round(box[1] * scale)
+                x_plus_w = round((box[0] + box[2]) * scale)
+                y_plus_h = round((box[1] + box[3]) * scale)
+                
+                label = f"{self.CLASSES[class_ids[index]]} ({scores[index]:.2f})"
+                color = self.random_colors[class_ids[index]]
+                cv.rectangle(self.yolo_im, (x, y), (x_plus_w, y_plus_h), self.random_colors[-1], 2)
+                cv.putText(self.yolo_im, label, (x - 10, y - 10), cv.FONT_HERSHEY_SIMPLEX, 1.8, color, 2)
+                cv.putText(self.yolo_im, label, (x - 10, y - 10), cv.FONT_HERSHEY_SIMPLEX, 1.8, (0, 0, 205), 2)
             
-            pixmap = QPixmap.fromImage(cv_mat_to_qimage(self.yolo_im))
-            # self.ui.label_v_src.setPixmap(pixmap.scaled(self.ui.label_v_src.size()))
-            self.ui.label_yolo_dst.setPixmap(pixmap.scaled(self.width()//2, int(self.height()/1.3)))
+            return self.yolo_im
 
     def draw_bounding_box(self, img, class_id, confidence, x, y, x_plus_w, y_plus_h):
         """
@@ -251,8 +359,9 @@ class Home_win(QMainWindow):
         """
         label = f"{self.CLASSES[class_id]} ({confidence:.2f})"
         color = self.random_colors[class_id]
-        cv.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+        cv.rectangle(img, (x, y), (x_plus_w, y_plus_h), self.random_colors[-1], 2)
         cv.putText(img, label, (x - 10, y - 10), cv.FONT_HERSHEY_SIMPLEX, 1.8, color, 2)
+        cv.putText(img, label, (x - 10, y - 10), cv.FONT_HERSHEY_SIMPLEX, 1.8, (0, 0, 205), 2)
         # cv.putText(img, label, (x - 10, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
 
@@ -293,7 +402,7 @@ class Home_win(QMainWindow):
         if self.img_path is not None and len(self.img_path):    
             # 如果选择了文件夹，则将其路径设置到lineEdit中
      
-            if self.img_path.endswith(tuple(self.format_lists)):
+            if self.img_path.endswith(tuple(self.img_format_lists)):
                 self.im = cv.imread(self.img_path)
                 if self.im is not None:
                     img = cv.cvtColor(self.im, cv.COLOR_BGR2RGB)
@@ -302,7 +411,7 @@ class Home_win(QMainWindow):
                     self.ui.label_v_src.setPixmap(pixmap.scaled(self.width()//2, int(self.height()/1.3)))
                     self.img_show = True
                     
-                    value_spin = self.ui.spinBox.value()
+                    value_spin = self.ui.spinBox_thresh.value()
                     print(value_spin)
                     self.threshold_set_img(value_spin)
                     
--- vision/onnx/fuse_conv_bn.py
+++ vision/onnx/fuse_conv_bn.py
@@ -11,14 +11,27 @@
 # import packets
 import os
 import onnx
+import onnxsim
+from onnxmltools.utils import float16_converter
+# 
 
 
 
+onnx_path = '../weights/yolov8n.onnx'
+
+
+model = onnx.load(onnx_path)
+
+print(model)
 
 
 
+save_folder = os.path.dirname(onnx_path)
+print('load model success!')
+trans_fp16_model = float16_converter.convert_float_to_float16(model, keep_io_types=True)
 
 
+onnx.save_model(trans_fp16_model, os.path.join(save_folder, 'yolov8_fp16.onnx'))
 
 
 
