ea64e32f61fe2d4a485bc6ecb9d8fc60567dc388
jmlw8023 <jmlw8023@163.com>
2024-06-02 13:08:14 +0800

    add

---
+++ web/streamlit/demo.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   demo.py
+@Time    :   2024/06/02 11:28:52
+@Author  :   jolly 
+@Version :   python3
+@Contact :   jmlw8023@163.com
+'''
+
+# import packets
+import os
+import numpy as np
+
+import streamlit as st
+
+
+
+
+st.title('页面')
+
+st.write('welcome use streamlit!')
+
+
+
+
+
+
