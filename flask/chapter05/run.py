e1c15265d878515330de63eae7db5cfbad651f96
jmlw8023 <jmlw8023@163.com>
2024-06-08 15:18:13 +0800

    repair

--- flask/chapter05/run.py
+++ flask/chapter05/run.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from re import T
 from unicodedata import name
 from app import create_app
@@ -12,22 +12,3 @@ if __name__ == '__main__':
     demo = create_app()
 
     demo.run(debug=True, port=5050)
-
-
-=======
-from re import T
-from unicodedata import name
-from app import create_app
-
-
-
-
-
-
-if __name__ == '__main__':
-    demo = create_app()
-
-    demo.run(debug=True, port=5050)
-
-
->>>>>>> 3efa523ff0c2328ea326da8c6ab7d5be45143578
