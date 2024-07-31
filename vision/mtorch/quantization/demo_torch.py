6f3e6f8014eecad1d4862f95d33ff33b314fd6c4
jmlw8023 <jmlw8023@163.com>
2024-07-31 11:22:50 +0800

    pytorch

--- vision/mtorch/quantization/demo_torch.py
+++ vision/mtorch/quantization/demo_torch.py
@@ -1,6 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File    :   demo_torch.py
+@Time    :   2024/07/18 09:10:27
+@Author  :   hgh 
+@Version :   1.0
+@Desc    :    
+'''
+
+# import module
 
 
 import torch
+
+import torch.nn as nn
+
+from torchvision.models import regnet
+
 from torch.quantization.observer import MinMaxObserver, MovingAverageMinMaxObserver, HistogramObserver
+
+
+
 
 
